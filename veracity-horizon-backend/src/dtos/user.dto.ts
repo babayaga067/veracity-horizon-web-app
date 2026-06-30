7b4c6576a6e0deecdb866c1bf5e1f18a2f32abed
kushmakar shahu <kushmakarshahu@gmail.com>
2026-06-30 21:30:12 +0545

    user request validation

--- veracity-horizon-backend/src/dtos/user.dto.ts
+++ veracity-horizon-backend/src/dtos/user.dto.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 import { UserSchema } from "../types/user.type";
 
-// DTO for creating a user (registration)
 export const CreateUserDTO = UserSchema.pick({
   firstName: true,
   lastName: true,
@@ -11,9 +10,31 @@ export const CreateUserDTO = UserSchema.pick({
 });
 export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
 
-// DTO for login
+export const AdminCreateUserDTO = UserSchema.pick({
+  firstName: true,
+  lastName: true,
+  email: true,
+  username: true,
+  password: true,
+  role: true,
+});
+export type AdminCreateUserDTO = z.infer<typeof AdminCreateUserDTO>;
+
 export const LoginUserDTO = UserSchema.pick({
   email: true,
   password: true,
 });
 export type LoginUserDTO = z.infer<typeof LoginUserDTO>;
+
+export const UpdateUserDTO = UserSchema.partial().pick({
+  firstName: true,
+  lastName: true,
+  email: true,
+  username: true,
+  password: true,
+  role: true,
+  profileImage: true,
+  fullName: true,
+  phoneNumber: true,
+});
+export type UpdateUserDTO = z.infer<typeof UpdateUserDTO>;
