47a97a952d8975ff801e0e71c22642a034c73c18
kushmakar shahu <kushmakarshahu@gmail.com>
2026-07-22 16:09:57 +0545

    removed backend build artifact user.controller.d.ts

--- veracity-horizon-backend/dist/controllers/admin/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export declare class AdminUserController {
-    createUser(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>>>;
-    updateUser(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>>>;
-    deleteUser(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>>>;
-    listUsers(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>>>;
-    getUserById(req: Request, res: Response, next: NextFunction): Promise<Response<any, Record<string, any>>>;
-}
-//# sourceMappingURL=user.controller.d.ts.map
